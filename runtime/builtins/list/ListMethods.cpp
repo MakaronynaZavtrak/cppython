587e466a0792737a62862658172677d3509a804d
Oleg Semenov <semyonov.owa@gmail.com>
2026-06-25 22:51:28 +0500

    Добавил поддержку __ge__ для list.

--- runtime/builtins/list/ListMethods.cpp
+++ runtime/builtins/list/ListMethods.cpp
@@ -122,6 +122,24 @@ namespace {
         );
     }
 
+    Value makeGreaterOrEqualMethod(const Value& obj) {
+
+        auto list = extract<Value::ListPtr>(obj);
+
+        return makeBuiltin(
+            "__ge__",
+
+            [list](const std::vector<Value> &args,
+                   const Kwargs &,
+                   const std::shared_ptr<Environment> &) -> Value {
+
+                expectArgs(args, 1, "__ge__");
+
+                return Value(list->greaterOrEqual(args[0]));
+            }
+        );
+    }
+
     Value makeAppendMethod(const Value& obj) {
 
         auto list = extract<Value::ListPtr>(obj);
@@ -390,6 +408,7 @@ namespace {
         REGISTER_METHOD("__ne__", makeNotEqualMethod),
         REGISTER_METHOD("__le__", makeLessOrEqualMethod),
         REGISTER_METHOD("__lt__", makeLessMethod),
+        REGISTER_METHOD("__ge__", makeGreaterOrEqualMethod),
         REGISTER_METHOD("append", makeAppendMethod),
         REGISTER_METHOD("pop", makePopMethod),
         REGISTER_METHOD("extend", makeExtendMethod),
