ed0dc9c83232d212a3d388da6e0dd92dfc81bdab
Oleg Semenov <semyonov.owa@gmail.com>
2026-06-25 22:57:33 +0500

    Добавил поддержку __gt__ для list.

--- runtime/builtins/list/ListMethods.cpp
+++ runtime/builtins/list/ListMethods.cpp
@@ -140,6 +140,24 @@ namespace {
         );
     }
 
+    Value makeGreaterMethod(const Value& obj) {
+
+        auto list = extract<Value::ListPtr>(obj);
+
+        return makeBuiltin(
+            "__gt__",
+
+            [list](const std::vector<Value> &args,
+                   const Kwargs &,
+                   const std::shared_ptr<Environment> &) -> Value {
+
+                expectArgs(args, 1, "__gt__");
+
+                return Value(list->greater(args[0]));
+            }
+        );
+    }
+
     Value makeAppendMethod(const Value& obj) {
 
         auto list = extract<Value::ListPtr>(obj);
@@ -409,6 +427,7 @@ namespace {
         REGISTER_METHOD("__le__", makeLessOrEqualMethod),
         REGISTER_METHOD("__lt__", makeLessMethod),
         REGISTER_METHOD("__ge__", makeGreaterOrEqualMethod),
+        REGISTER_METHOD("__gt__", makeGreaterMethod),
         REGISTER_METHOD("append", makeAppendMethod),
         REGISTER_METHOD("pop", makePopMethod),
         REGISTER_METHOD("extend", makeExtendMethod),
